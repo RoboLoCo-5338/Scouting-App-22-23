b631aa82256ef0ac2bb6102e02547e5f6439ab16
aedan07 <40202903+aedan07@users.noreply.github.com>
2023-03-28 16:16:25 -0400

    Fixed error.

--- ScoutingApp2023/MainPage.xaml.cs
+++ ScoutingApp2023/MainPage.xaml.cs
@@ -13,5 +13,23 @@ public partial class MainPage : ContentPage
         MauiProgram.ViewModel.SidePicker = side;
         MauiProgram.ViewModel.MatchNumberEntry = matchNumberEntry;
     }
+
+    private void OnAlliancePickerSelectedIndexChanged(object sender, EventArgs e)
+    {
+        Picker picker = (Picker)sender;
+        int selectedIndex = picker.SelectedIndex;
+        if (selectedIndex < 0)
+        {
+            MauiProgram.ViewModel.FlowDirection = FlexDirection.Row;
+            return;
+        }
+        string item = picker.Items[selectedIndex];
+        if (item == "Right")
+        {
+            MauiProgram.ViewModel.FlowDirection = FlexDirection.RowReverse;
+            return;
+        }
+        MauiProgram.ViewModel.FlowDirection = FlexDirection.Row;
+    }
 }
 
